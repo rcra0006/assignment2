5faf3be72d3d6db5b204a50f65758ad2bf057a9f
RNM2022\Rodney <rcra0006@student.monash.edu>
2024-10-13 18:34:43 +1100

    added data transformed for sankey file

---
+++ data/read.m
@@ -0,0 +1,23 @@
+clc; clear all; close all;
+
+T = readtable('speciesdata.csv');
+
+A = table;
+A.Order = T.Order;
+A.Class = T.Class;
+A.Species = T.Species;
+
+
+[lights,ia,ic]=unique(A.Order);
+nrows = accumarray(ic,1);
+B = cell2table([lights num2cell(nrows)],'VariableNames', {'Order' 'nrows'});
+
+C = table;
+C.Order = T.Order;
+C.Class = T.Class;
+C = unique(C);
+
+F = join(C, B, 'Keys','Order','KeepOneCopy','Class');
+
+writetable(F,'rows.csv','Delimiter',',','QuoteStrings','all')
+type 'rows.csv';
